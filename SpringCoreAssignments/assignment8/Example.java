d1e43ebf069972f1d53cb18ae2b8874c6f878690
MuskaanSingh09 <86997968+MuskaanSingh09@users.noreply.github.com>
2021-08-02 16:35:28 +0530

    assignment8 committed

---
+++ SpringCoreAssignments/assignment8/Example.java
@@ -0,0 +1,39 @@
+package com.assignment8;
+
+public class Example 
+{
+  private String Subject;
+
+public String getSubject() 
+{
+	return Subject;
+}
+
+public void setSubject(String subject) 
+{
+	Subject = subject;
+}
+
+public Example() {
+	super();
+	// TODO Auto-generated constructor stub
+}
+
+@Override
+public String toString() 
+{
+	return "Example [Subject=" + Subject + "]";
+}
+  
+  public void init()
+  {
+	System.out.println("starting method");  
+  }
+ 
+  public void destroy()
+  {
+	System.out.println("ending method");  
+  }
+  
+}
+
